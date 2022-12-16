a0de64c111a794d4f2205c15dfc8d68bb30ebc19
Eganin <egorzaharin12@gmail.com>
2022-12-16 19:03:40 +0500

    add custom theme

--- app/src/main/java/com/best/easymind/ui/theme/Color.kt
+++ app/src/main/java/com/best/easymind/ui/theme/Color.kt
@@ -2,7 +2,22 @@ package com.best.easymind.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val baseLightPalette = EasyMindColors(
+    primaryText = Color(0xFF252323),
+    primaryBackground = Color(0xFFF1F7F6),
+    secondaryText = Color(0xFF828282),
+    secondaryBackground = Color(0xFF009688),
+    tintColor = Color(0xFF009688),
+    errorColor = Color(0xFFE91E63),
+    toolBarText = Color(0xFFF1F7F6)
+)
+
+val baseDarkPalette = EasyMindColors(
+    primaryText = Color(0xFFFFFFFF),
+    primaryBackground = Color(0xFF2B2E2E),
+    secondaryText = Color(0xFF2196F3),
+    secondaryBackground = Color(0xFF009688),
+    tintColor = Color(0xFF009688),
+    errorColor = Color(0xFFE91E63),
+    toolBarText = Color(0xFFF1F7F6)
+)
--- app/src/main/java/com/best/easymind/ui/theme/Theme.kt
+++ app/src/main/java/com/best/easymind/ui/theme/Theme.kt
@@ -5,40 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 @Composable
 fun EasyMindTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
     val colors = if (darkTheme) {
-        DarkColorPalette
+        baseDarkPalette
     } else {
-        LightColorPalette
+        baseLightPalette
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+    CompositionLocalProvider(
+        LocalEasyMindColors provides colors,
+        LocalEasyMindTypography provides Typography,
+        LocalEasyMindShapes provides Shapes,
         content = content
     )
 }
